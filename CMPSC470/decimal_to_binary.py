af6f16aa6e2b70ea053edcf87f2bf364c768c9d0
afeins19 <56986596+afeins19@users.noreply.github.com>
2024-02-22 15:29:35 -0500

    123
    123

---
+++ CMPSC470/decimal_to_binary.py
@@ -0,0 +1,18 @@
+def decimal_to_binary(val):
+    bin_val = []
+    if val == 0:
+        return 0
+
+    if val == 1:
+        return 1
+
+    while val!=0:
+        cur_bit = val % 2
+        bin_val = [str(cur_bit)] + bin_val
+        val = val // 2
+
+
+
+    return ''.join(bin_val)
+
+print(decimal_to_binary(46))
