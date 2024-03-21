5a39976ab62c795408d592bf8a6f574a845cca18
afeins19 <56986596+afeins19@users.noreply.github.com>
2024-03-21 10:03:57 -0400

    123

---
+++ CMPSC413/lab_5_v2/huffman_tree.py
@@ -0,0 +1,83 @@
+import heapq
+
+# this class will represent the huffman tree node
+class HuffmanTree:
+    def __init__(self, priority, char, l_child=None, r_child=None):
+        self.priority = priority
+        self.char = char
+        self.l_child = l_child
+        self.r_child =r_child
+        self.position = ''
+
+
+    # heapq uses this to measure the priority
+    def __lt__(self, other):
+        return self.priority < other.priority
+
+    def __str__(self):
+        return f"{self.char}"
+
+
+# function for creating huffman tree nodes
+def make_huffman_tree(char_count) -> []: # [(char, freq), ...]
+    min_heap = []
+
+    # adds all the nodes into the priority queue
+    for char, freq in char_count.items():
+        node = HuffmanTree(priority=freq, char=char)
+        heapq.heappush(min_heap, node)
+
+    while len(min_heap) > 1:
+        l_child = heapq.heappop(min_heap)
+        r_child = heapq.heappop(min_heap)
+
+        l_child.position = '0'
+        r_child.position = '1'
+
+        internal_node = HuffmanTree(
+            priority=(l_child.priority + r_child.priority),
+            char=str(l_child.char+r_child.char),
+            l_child=l_child,
+            r_child=r_child
+        )
+
+        heapq.heappush(min_heap, internal_node)
+
+    return min_heap[0]
+
+def get_huffman_codes(node: HuffmanTree, code='', code_dict=None) -> dict:
+    if code_dict is None:
+        code_dict = {}
+
+    if not node.l_child and not node.r_child:
+        code_dict[node.char] = code
+
+    if node.l_child:
+        get_huffman_codes(node.l_child, code + '0', code_dict)
+
+    if node.r_child:
+        get_huffman_codes(node.r_child, code + '1', code_dict)
+
+    return code_dict
+
+
+def make_char_freqList(str):
+    char_count = dict()
+
+    for char in list(str):
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+
+    return char_count
+
+
+
+msg = "AAAABBBCCBABCCBABAABCBAABCEEEEDDDDFFFFEFECCCCCBBBAAA"
+char_count = make_char_freqList(msg)
+
+huffman_tree = make_huffman_tree(char_count)
+
+print(get_huffman_codes(huffman_tree))
+
