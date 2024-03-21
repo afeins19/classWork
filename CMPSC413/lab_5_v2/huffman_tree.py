d58033bbd2d63a73aa7fa3d699bb1e60bdccfe1f
afeins19 <56986596+afeins19@users.noreply.github.com>
2024-03-21 10:49:43 -0400

    123

--- CMPSC413/lab_5_v2/huffman_tree.py
+++ CMPSC413/lab_5_v2/huffman_tree.py
@@ -60,7 +60,30 @@ def get_huffman_codes(node: HuffmanTree, code='', code_dict=None) -> dict:
 
     return code_dict
 
+def encode_message(msg, codes) -> str:
+    encoded_message = ""
 
+    for char in msg:
+        encoded_message+=codes[char]
+
+    return encoded_message
+
+def decode_message(msg, codes) -> str:
+    decoded_message=""
+
+    rev_codes = {v: k for k,v in codes.items()}
+
+    # parses the message until a huffman code is found for the bit pattern
+    while msg:
+        for i in range(1, len(msg) + 1):
+            code = msg[:i]
+
+            if code in rev_codes:
+                decoded_message += rev_codes[code]
+                msg = msg[i:]
+                break
+
+    return decoded_message
 def make_char_freqList(str):
     char_count = dict()
 
@@ -73,11 +96,43 @@ def make_char_freqList(str):
     return char_count
 
 
+# --- Task 1: Testing with message
+msg = input("Please enter a message: ")
 
-msg = "AAAABBBCCBABCCBABAABCBAABCEEEEDDDDFFFFEFECCCCCBBBAAA"
 char_count = make_char_freqList(msg)
-
 huffman_tree = make_huffman_tree(char_count)
+huffman_codes = get_huffman_codes(huffman_tree)
+e_msg = encode_message(msg, huffman_codes)
 
-print(get_huffman_codes(huffman_tree))
+print(f"Input: {msg}")
+print(f"Encoded: {e_msg}\n")
+print(f"Guide: ")
+
+for k,v in huffman_codes.items():
+    print(f"{k}:{v}")
+
+
+# encoding the file
+with open("user_message.txt", "r+") as file:
+    msg = ""
+    for line in file.readlines():
+        msg += line
+
+    char_count = make_char_freqList(msg)
+    huffman_tree = make_huffman_tree(char_count)
+    huffman_codes = get_huffman_codes(huffman_tree)
+
+    with open("encoded.txt", "w+") as encoded_file:
+        e_msg = encode_message(msg, huffman_codes)
+        print(f"\n\nencoded message: {e_msg}")
+        encoded_file.write(e_msg)
+
+# decoding the file
+with open("encoded.txt", "r+") as decode_file:
+    msg=decode_file.read()
+
+    d_msg = decode_message(msg, huffman_codes)
+    print(f"Decoded Message: {d_msg}")
+
+
 
