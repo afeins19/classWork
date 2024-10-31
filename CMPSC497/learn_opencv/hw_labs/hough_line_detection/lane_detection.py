7988474306e1c534de5d65cec390c651a1df12d8
Aaron Feinberg <afeinberg35@gmail.com>
2024-10-31 08:08:10 -0400

    123

--- CMPSC497/learn_opencv/hw_labs/hough_line_detection/lane_detection.py
+++ CMPSC497/learn_opencv/hw_labs/hough_line_detection/lane_detection.py
@@ -1,18 +1,19 @@
 import cv2 as cv
 import numpy as np
 import math
+import time
 
-def detectLines(img_loc):
+def detectLines(img_loc, sensitivity):
     # loading image 
     img = cv.imread(img_loc, cv.IMREAD_GRAYSCALE) # convert to gray
-
+    
     canny = cv.Canny(img, 50, 200, None, 3)
 
     # save edges to be displayed later im final image 
     img_edges = cv.cvtColor(canny, cv.COLOR_GRAY2BGR)
     
     # apply hough transform 
-    lines_detected = cv.HoughLines(canny, 1, np.pi /180, 150, None, 0, 0)
+    lines_detected = cv.HoughLines(canny, 1, np.pi /180, sensitivity, None, 0, 0)
 
     # constructing accumulator matrix
     accum = []
@@ -35,10 +36,23 @@ def detectLines(img_loc):
             # generate points and plot 
             pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
             pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
-            cv.line(img_edges, pt1, pt2, (0,0,255), 3, cv.LINE_AA)
+            cv.line(img_edges, pt1, pt2, (15,230,230), 2, cv.LINE_AA)
 
-        cv.imshow("Detected Lines: {str(len(lines))}", img)
+        print(f"Lines={len(accum)}")
+           
+    else:
+        print("no lines")
+
+    cv.imshow("Original:", img)
+    cv.imshow("Detected Lines: {str(len(lines))}", img_edges)
+
+    while True:
+        k= cv.waitKey(1) & 0xFF
+        if k== 27: # esc
+            cv.destroyAllWindows()
 
 if __name__ == "__main__":
-    IMG_LOC = '/home/aaron/Projects/classWork/CMPSC497/learn_opencv/hw_labs/hough_line_detection/test/highway.jpg'
-    detectLines(IMG_LOC)
+    import os
+    os.environ["QT_QPA_PLATFORM"] = "xcb" # linux issue
+    IMG_LOC = '/home/aaron/Projects/classWork/CMPSC497/learn_opencv/hw_labs/hough_line_detection/test/goog.png'
+    detectLines(IMG_LOC, sensitivity=235)
